c184c231bcda8ba48ca46ed10cf73aea079cbd62
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-10 17:00:40 +0200

    adding images

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,38 +9,101 @@ const gridItems = [
     },
     {
         author: 'Johannes Vermeer',
+        containerClasses: 'grid__image-container--large',
         title: 'Girl with a Pearl Earring',
-        smallImage: '/assets/images/starry-night/hero-small.jpg',
-        largeImage: '/assets/images/starry-night/hero-large.jpg'
+        smallImage: '/assets/images/girl-with-pearl-earring/hero-small.jpg',
+        largeImage: '/assets/images/girl-with-pearl-earring/hero-large.jpg'
     },
     {
         author: 'Pablo Picass',
         title: 'Guernica',
-        smallImage: '/assets/images/starry-night/hero-small.jpg',
-        largeImage: '/assets/images/starry-night/hero-large.jpg'
+        smallImage: '/assets/images/guernica/hero-small.jpg',
+        largeImage: '/assets/images/guernica/hero-large.jpg'
     },
     {
         author: 'Artemisia Gentileschi',
         title: 'Penitent Magdalenet',
-        smallImage: '/assets/images/starry-night/hero-small.jpg',
-        largeImage: '/assets/images/starry-night/hero-large.jpg'
+        smallImage: '/assets/images/penitent-magdalene/hero-small.jpg',
+        largeImage: '/assets/images/penitent-magdalene/hero-large.jpg'
     },
     {
         author: 'Rembrandt',
         title: 'The Storm on the Sea of Galilee',
+        smallImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-small.jpg',
+        largeImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-large.jpg'
+    },
+    {
+        author: 'Vincent Van Gogh',
+        title: 'Starry Night',
         smallImage: '/assets/images/starry-night/hero-small.jpg',
         largeImage: '/assets/images/starry-night/hero-large.jpg'
     },
+    {
+        author: 'Johannes Vermeer',
+        containerClasses: 'grid__image-container--large',
+        title: 'Girl with a Pearl Earring',
+        smallImage: '/assets/images/girl-with-pearl-earring/hero-small.jpg',
+        largeImage: '/assets/images/girl-with-pearl-earring/hero-large.jpg'
+    },
+    {
+        author: 'Pablo Picass',
+        title: 'Guernica',
+        smallImage: '/assets/images/guernica/hero-small.jpg',
+        largeImage: '/assets/images/guernica/hero-large.jpg'
+    },
+    {
+        author: 'Artemisia Gentileschi',
+        title: 'Penitent Magdalenet',
+        smallImage: '/assets/images/penitent-magdalene/hero-small.jpg',
+        largeImage: '/assets/images/penitent-magdalene/hero-large.jpg'
+    },
+    {
+        author: 'Rembrandt',
+        title: 'The Storm on the Sea of Galilee',
+        smallImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-small.jpg',
+        largeImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-large.jpg'
+    },
+    {
+        author: 'Vincent Van Gogh',
+        title: 'Starry Night',
+        smallImage: '/assets/images/starry-night/hero-small.jpg',
+        largeImage: '/assets/images/starry-night/hero-large.jpg'
+    },
+    {
+        author: 'Johannes Vermeer',
+        containerClasses: 'grid__image-container--large',
+        title: 'Girl with a Pearl Earring',
+        smallImage: '/assets/images/girl-with-pearl-earring/hero-small.jpg',
+        largeImage: '/assets/images/girl-with-pearl-earring/hero-large.jpg'
+    },
+    {
+        author: 'Pablo Picass',
+        title: 'Guernica',
+        smallImage: '/assets/images/guernica/hero-small.jpg',
+        largeImage: '/assets/images/guernica/hero-large.jpg'
+    },
+    {
+        author: 'Artemisia Gentileschi',
+        title: 'Penitent Magdalenet',
+        smallImage: '/assets/images/penitent-magdalene/hero-small.jpg',
+        largeImage: '/assets/images/penitent-magdalene/hero-large.jpg'
+    },
+    {
+        author: 'Rembrandt',
+        title: 'The Storm on the Sea of Galilee',
+        smallImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-small.jpg',
+        largeImage: '/assets/images/the-storm-on-the-sea-of-galilee/hero-large.jpg'
+    },
 ];
 
-const createElement = ({ author, title, }) => {
+const createElement = ({ author, containerClasses, title, smallImage }) => {
     return (
         `
             <a class="mb-3" href="/">
-                <figure class="grid__image-container position-relative">
+                <figure class="grid__image-container position-relative ${containerClasses}">
                     <img
                         class="d-block h-100 w-100 grid__image"
-                        src="/assets/images/starry-night/hero-small.jpg"
+                        src=${smallImage}
                     />
                     <figcaption class="position-absolute left-0 bottom-0 w-100 pt-5 grid__image-caption px-3">
                         <h2 class="text-light">${ title }</h2>
