30db65a106924d96017ab825217f2bf9b749727d
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-14 11:53:27 +0200

    getting item details by url ID

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,7 +1,18 @@
 
 
+
+const fetchData = (id) => {
+    console.log(id)
+    fetch('../../data.json')
+        .then(res => res.json())
+        .then(data => console.log(data.find((item, index) => index === id)))
+        .catch(console.log)
+};
+
 const start = () => {
-    console.log('hello RT')
+    const url = document.location.href;
+    const id = parseInt(url.split('?')[1])
+    fetchData(id)
 };
 
 window.addEventListener('load', start);
