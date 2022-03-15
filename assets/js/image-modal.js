ac2b5f99432c8240142904f7a892d1798c09e964
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-15 14:11:17 +0200

    blocking windows scroll event when image-modal is open

--- assets/js/image-modal.js
+++ assets/js/image-modal.js
@@ -1,13 +1,21 @@
 let modal = null;
 let closeButton = null;
 let openButton = null;
+let bodyElement = null;
 
 const closeHandler = ({ addClass, removeClass }) => () => {
     modal.classList.remove(removeClass);
     modal.classList.add(addClass);
+
+    if(removeClass === 'd-none') {
+        bodyElement.classList.add('overflow-hidden');
+    } else {
+        bodyElement.classList.remove('overflow-hidden');
+    }
 };
 
 const modalStart = () => {
+    bodyElement = document.querySelector('body');
     modal = document.querySelector('[data-modal=image]');
     closeButton = document.querySelector('[data-button=close-modal]');
     openButton = document.querySelector('[data-button=open-modal]');
